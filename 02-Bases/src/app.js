c8bd75c7daa282a5202a48f29f2bfc80cb4e9ab7
Eternalsoull <victor.3801920653@ucaldas.edu.co>
2025-02-09 18:23:30 -0500

    learning about factory functions and adapters

--- 02-Bases/src/app.js
+++ 02-Bases/src/app.js
@@ -1,18 +1,17 @@
+const{getAge, getUUID} = require('./plugins');
+
 // const {emailTemplate} =  require('./js-foundation/01-template');
 // require('./js-foundation/02-destructuring');
 // const {getUserById} =  require('./js-foundation/03-callbacks');
-const {getUserById} =  require('./js-foundation/03-callbacks');
+// const {getUserById} =  require('./js-foundation/03-callbacks');
+const {buildMakePerson } = require('./js-foundation/05-factory');
 
+const makePerson = buildMakePerson({getUUID, getAge});  
+
+const obj = { name: 'John', birthdate: '2000-10-14' };
+
+const john = makePerson(obj);
+
+console.log(john);
 
 // console.log(emailTemplate);
-
-const id = 3;
-
-getUserById(id, (error, user) => {
-    if(error){
-        throw new Error(error);
-    }
-    console.log(user);
-});
-
-// se agrega un comentario
